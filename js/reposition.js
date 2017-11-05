d6eee6c010b42dad354f74951fdc527d1a692e6e
LAPTOP-Q5HR0407\brandon <branvh@gmail.com>
2017-11-05 12:04:53 -0600

    add row text

--- js/reposition.js
+++ js/reposition.js
@@ -1,9 +1,40 @@
-let respositionRowNames = [
+let respositionRowNames = ['Cash & Non-IB Balances',
+    'FFS & IB Balances',
+    'Investments',
+    'Net Loans',
+    'OREO',
+    'Intangible Assets',
+    'Other Assets',
+    'Total Assets',
+    'Total Deposits',
+    'Non-Core Funding',
+    'Other Liabilities',
+    'Total Liabilities',
+    'Total Equity',
+    'Total Liabilities & Equity',
+    'Int Inc: FFS, IB Balances, Other',
+    'Investment Income',
+    'Tax. Equiv. Adjustment',
+    'Int. Inc: Loans',
+    'Total Interest Income',
+    'Total Interest Expense',
+    'Net Interest Income',
+    'Loan Loss Provision',
+    'Non-Interest Income',
+    'Non-Interest Expense',
+    'Pre-Tax Operating Income',
+    'Realized Gains',
+    'Pre-Tax Income',
+    'Provision for Taxes',
+    'Net Income',
+    'Additional Revenue',
+    'Additional Income',
+    'yld'
+]
 
-"Asset1",
-"Asset2",
-"Asset3"
-
+boldFieldNames = [
+'Total Assets',
+'Net Income'
 ]
 
 $(document).ready(function() {
@@ -13,9 +44,19 @@ $(document).ready(function() {
     let fieldArr = $(".fieldNames")
 
     $.each(fieldArr, function(index, value) {
-        
-    	//fieldArr[index].text(respositionRowNames[index])
+
+        //fieldArr[index].text(respositionRowNames[index])
         fieldArr[index].append(respositionRowNames[index])
+
+        //console.log(boldFieldNames.indexOf(respositionRowNames[index]))
+
+        //apply condition formatting
+        if (boldFieldNames.indexOf(respositionRowNames[index]) != -1) {
+
+        	console.log(fieldArr[index] + ' will be bold')
+        	//fieldArr[index].css('color: red; font-weight: bold')
+
+        }
 
     });
 
