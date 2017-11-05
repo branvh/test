2d6a2ce9d25df4e1e04c4d71ad813792cd16db6b
LAPTOP-Q5HR0407\brandon <branvh@gmail.com>
2017-11-05 11:37:25 -0600

    basic setup

--- js/reposition.js
+++ js/reposition.js
@@ -1,5 +1,22 @@
+let respositionRowNames = [
+
+"Asset1",
+"Asset2",
+"Asset3"
+
+]
+
 $(document).ready(function() {
 
-console.log('success')
+    console.log('success')
+
+    let fieldArr = $(".fieldNames")
+
+    $.each(fieldArr, function(index, value) {
+        
+    	//fieldArr[index].text(respositionRowNames[index])
+        fieldArr[index].append(respositionRowNames[index])
+
+    });
 
 })
